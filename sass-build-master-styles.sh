f4ef3fa428ab858d8dc76055aaa71cccf37f10c1
Zed <weizheng.tan@gmail.com>
2018-05-01 19:55:46 +0800

    update utility scripts

--- sass-build-master-styles.sh
+++ sass-build-master-styles.sh
@@ -1,4 +1,4 @@
-source_dir='_theme/nsdmdh/_static/sass'
-dest_dir='_theme/nsdmdh/_static'
+source_dir='source/_theme/nsdmdh/_static/sass'
+dest_dir='source/_theme/nsdmdh/_static'
 
 sass --update $source_dir:$dest_dir --style compressed --trace
--- sass-watch-master-styles.sh
+++ sass-watch-master-styles.sh
@@ -1,4 +1,4 @@
-source_dir='_theme/nsdmdh/_static/sass'
-dest_dir='_theme/nsdmdh/_static'
+source_dir='source/_theme/nsdmdh/_static/sass'
+dest_dir='source/_theme/nsdmdh/_static'
 
 sass --watch $source_dir:$dest_dir --style compressed --trace
