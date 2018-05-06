f7aab8b0fc7e688c00cb89f701b4e3f708ebcbcc
Zed <weizheng.tan@gmail.com>
2018-05-06 18:40:32 +0800

    removed css files and added to gitignore; now only contains scss and sass files; added hack for hiding local toc entries in sidebar

--- source/_theme/nsdmdh/_static/js/nav.js
+++ source/_theme/nsdmdh/_static/js/nav.js
@@ -35,4 +35,58 @@ nav_burger.forEach((burger)=>{
     burger.addEventListener('mousedown',nav_event)
 })
 
+!function(){
+// Dropdown event listener
+const nav_dropdown_array = document.querySelectorAll('.has-dropdown')
+
+const nav_toggle = (target)=>{
+  const nav_event = (event)=>{
+    if(event.type==='mouseenter'||event.type==='mouseleave'){
+      if(target.classList.contains('is-active')){
+        target.classList.remove('is-active')
+      } else { target.classList.add('is-active')}
+    }
+  }
+  // Check if hamburger is activated. If activated, then should not trigger menu dropdowns
+  if(document.querySelector('.navbar-menu').classList.contains('is-active')){}else{
+    target.addEventListener('mouseenter',nav_event)
+    target.addEventListener('mouseleave',nav_event)
+  }
+}
+
+nav_dropdown_array.forEach(nav_toggle)
+
+// Hamburger menu event listener
+
+const nav_burger = document.querySelectorAll('.navbar-burger')
+
+nav_burger.forEach((burger)=>{
+    const nav_event = (event)=>{
+      const target = document.querySelector('.navbar-menu').classList
+      if(event.type==='mousedown'){
+        if(target.contains('is-active')){
+          target.remove('is-active')
+        } else { target.add('is-active')}
+      }
+    }
+    burger.addEventListener('mousedown',nav_event)
+})
+
+// Hide local toc entries in sidebar
+const internalLinks = document.querySelector('.sphinxsidebar').querySelectorAll('a[href^="#"]')
+for (i in internalLinks){
+  if (i == 0){ 
+    // do nothing
+  } else {
+    try {
+    internalLinks[i].parentNode.classList.add('hidden')
+    } catch {
+      console.log('caught')
+    }
+  }
+}
+
+}()
+
+
 }()
